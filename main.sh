9628f2c5af89c4cc265f5e4fea926f93e7610260
himalaychava <87822540+himalaychava@users.noreply.github.com>
2024-12-28 11:17:07 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,5 +1,3 @@
-
-
 oc create -f https://raw.githubusercontent.com/cloud-native-toolkit/deployer-osdu/refs/heads/main/M22/pipeline.yaml -n default
 sleep 60
 oc create -f https://raw.githubusercontent.com/cloud-native-toolkit/deployer-osdu/refs/heads/main/M22/pipeline-run.yaml -n default
